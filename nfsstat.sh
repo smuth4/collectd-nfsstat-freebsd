b42ccc4604449f6217b32f7b1c9b09c2737ce0fe
Stephen Muth <smuth4@users.noreply.github.com>
2015-11-29 03:46:33 -0500

    Create nfsstat.sh

---
+++ nfsstat.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+HOSTNAME="${COLLECTD_HOSTNAME:-$(hostname -f)}"
+INTERVAL="${COLLECTD_INTERVAL:-10}"
+
+# Local settings
+CLIENT_STATS=true
+SERVER_STATS=true
+CLIENT_DATATYPE='derive'
+SERVER_DATATYPE='derive'
+
+while sleep "$INTERVAL"; do
+  if [ $SERVER_STATS = true ]; then
+     for i in {1..8}; do
+       nfsstat -e -s | grep -v ':' | grep -v '^$' | awk "{print \$$i}" \
+         | paste - - | sed '/^[[:space:]]*$/d' | sort | tr '[:upper:]' '[:lower:]' \
+         | while read -r key value; do
+         echo "PUTVAL $HOSTNAME/nfsstat/${SERVER_DATATYPE}-server-${key} interval=$INTERVAL N:$value"
+       done
+     done
+  fi
+  if [ $CLIENT_STATS = true ]; then
+     for i in {1..8}; do
+       nfsstat -e -c | grep -v ':' | grep -v '^$' | awk "{print \$$i}" \
+         | paste - - | sed '/^[[:space:]]*$/d' | sort | tr '[:upper:]' '[:lower:]' \
+         | while read -r key value; do
+         echo "PUTVAL $HOSTNAME/nfsstat/${CLIENT_DATATYPE}-client-${key} interval=$INTERVAL N:$value"
+       done
+     done
+  fi
+done
