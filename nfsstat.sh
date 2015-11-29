b94159fd01186078ebcbf098ca8a47130603454b
Stephen Muth <smuth4@gmail.com>
2015-11-29 18:00:17 -0500

    Fix indenting

--- nfsstat.sh
+++ nfsstat.sh
@@ -10,23 +10,23 @@ SERVER_DATATYPE='derive'
 
 while sleep "$INTERVAL"; do
   if [ $SERVER_STATS = true ]; then
-     stats="$(nfsstat -e -s)"
-     for i in {1..8}; do
-       echo "$stats" | grep -v ':' | grep -v '^$' | awk "{print \$$i}" \
-         | paste - - | sed '/^[[:space:]]*$/d' | sort | tr '[:upper:]' '[:lower:]' \
-         | while read -r key value; do
+    stats="$(nfsstat -e -s)"
+    for i in {1..8}; do
+      echo "$stats" | grep -v ':' | grep -v '^$' | awk "{print \$$i}" \
+        | paste - - | sed '/^[[:space:]]*$/d' | sort | tr '[:upper:]' '[:lower:]' \
+        | while read -r key value; do
          echo "PUTVAL $HOSTNAME/nfsstat/${SERVER_DATATYPE}-server-${key} interval=$INTERVAL N:$value"
-       done
-     done
+      done
+    done
   fi
   if [ $CLIENT_STATS = true ]; then
-     stats="$(nfsstat -e -c)"
-     for i in {1..8}; do
-       echo "$stats"  | grep -v ':' | grep -v '^$' | awk "{print \$$i}" \
-         | paste - - | sed '/^[[:space:]]*$/d' | sort | tr '[:upper:]' '[:lower:]' \
-         | while read -r key value; do
-         echo "PUTVAL $HOSTNAME/nfsstat/${CLIENT_DATATYPE}-client-${key} interval=$INTERVAL N:$value"
-       done
-     done
+    stats="$(nfsstat -e -c)"
+    for i in {1..8}; do
+      echo "$stats"  | grep -v ':' | grep -v '^$' | awk "{print \$$i}" \
+        | paste - - | sed '/^[[:space:]]*$/d' | sort | tr '[:upper:]' '[:lower:]' \
+        | while read -r key value; do
+        echo "PUTVAL $HOSTNAME/nfsstat/${CLIENT_DATATYPE}-client-${key} interval=$INTERVAL N:$value"
+      done
+    done
   fi
 done
